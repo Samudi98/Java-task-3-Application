941c87230987e0fda47a6324dd9e28206221b997
Samitha Perera <91825809+Ketha95@users.noreply.github.com>
2021-10-03 15:51:02 +0530

    Added name and corrected the variable name

--- Task3.java
+++ Task3.java
@@ -1,6 +1,9 @@
 public class Task3{
   public static void main(String[] args) {
     int x = 100 + 50;
-    System.out.println(y);
+    String name = "Welcome to Soysa Education";
+    System.out.println(x);
+    System.out.println(name);
+
   }
 }
